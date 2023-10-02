cc9831c687047d254e601deafc0d5af9aeb8c38a
borg323 <borg323@users.noreply.github.com>
2023-10-03 02:56:33 +0300

    simplify build with fixed_tt

--- src/search/dag_classic/node.h
+++ src/search/dag_classic/node.h
@@ -27,8 +27,6 @@
 
 #pragma once
 
-#include <absl/container/flat_hash_map.h>
-
 #include <algorithm>
 #include <cmath>
 #include <cstring>
@@ -43,6 +41,10 @@
 #include "neural/backend.h"
 #include "utils/cache.h"
 #include "utils/mutex.h"
+
+#ifndef FIX_TT
+#include <absl/container/flat_hash_map.h>
+#endif
 
 namespace lczero {
 namespace dag_classic {
