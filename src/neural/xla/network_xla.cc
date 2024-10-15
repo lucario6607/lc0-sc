6921ca9a7eaf874b806804c70376c987907cbdd9
borg323 <borg323@users.noreply.github.com>
2024-10-16 01:56:05 +0300

    make it work

--- src/neural/xla/network_xla.cc
+++ src/neural/xla/network_xla.cc
@@ -303,9 +303,10 @@ std::unique_ptr<Network> MakeXlaNetwork(const std::optional<WeightsFile>& w,
         WeightsToOnnxConverterOptions::StringToDataType(
             opts.GetOrDefault<std::string>("datatype", "f32"));
     onnx_converter_options.opset = opts.GetOrDefault<int>("opset", 18);
-    onnx_converter_options.alt_mish = opts.GetOrDefault<bool>("alt_mish", 18);
+    onnx_converter_options.alt_mish =
+        opts.GetOrDefault<bool>("alt_mish", false);
     onnx_converter_options.alt_layernorm =
-        opts.GetOrDefault<bool>("alt_layernorm", 18);
+        opts.GetOrDefault<bool>("alt_layernorm", false);
     auto converted = ConvertWeightsToOnnx(*w, onnx_converter_options);
     options = FillXlaRunnerFromOnnx(converted.onnx_model(), runner.get(),
                                     max_batch_size, steps, io_type);
