4618cb6d33db67d6275c96e4155e2a72afd97e27
borg323 <borg323@users.noreply.github.com>
2024-10-30 13:50:03 +0200

    make android pro apk

--- src/utils/optionsparser.cc
+++ src/utils/optionsparser.cc
@@ -140,9 +140,7 @@ bool OptionsParser::ProcessAllFlags() {
 
 bool OptionsParser::ProcessFlags(const std::vector<std::string>& args) {
   auto show_help = false;
-  if (CommandLine::BinaryName().find("pro") != std::string::npos) {
-    ShowHidden();
-  }
+  ShowHidden();
   for (auto iter = args.begin(), end = args.end(); iter != end; ++iter) {
     std::string param = *iter;
     if (param == "--show-hidden") {
